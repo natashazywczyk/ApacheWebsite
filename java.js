7883dbdb27f43b5a18c88922c9e310a6a3cc4589
NATASHA ZYWCZYK - STUDENT <g00415696@atu.ie>
2023-04-25 10:41:30 +0100

    changed layout, added external javascript file and css file Added more javascript prompts and style to existing html page

---
+++ java.js
@@ -0,0 +1,37 @@
+//alert("<hr>Hello from browser");
+//document.write("<br>Test");
+
+alert("This is my first page");
+
+let x = 0;
+x = "test";
+
+var y = "2";
+
+const w = 12;
+
+x = Number(y) + 3;
+//alert(x);
+
+function msg(){
+	alert("Test");
+}
+
+//msg();
+let answer = prompt("Enter a number");
+
+if(answer == 3){
+	alert("Yes it is 3");
+}else{
+	alert("No it is not 3");
+}
+
+
+let count = 1;
+while(count <= 3){
+	alert(count);
+	count++;
+}
+for(let i = 1; i <= 4; i++){
+	alert(i);
+}
